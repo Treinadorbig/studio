1c8384be58996b22e1b112078b2dac506b961923
TREINADOR BIG <treinadorbig@gmail.com>
2025-06-10 02:16:45 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,9 +1,10 @@
+
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, value, ...props }, ref) => { // Destructure value, other props go into ...props
     return (
       <input
         type={type}
@@ -12,7 +13,8 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
           className
         )}
         ref={ref}
-        {...props}
+        value={value ?? ''} // Ensure value is at least an empty string
+        {...props} // Spread the rest of the props
       />
     )
   }
